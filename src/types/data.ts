2d24fbe42db52a5809ce18df1db46f779aa4dc91
sgeunhi <alsrb582@snu.ac.kr>
2024-12-09 16:05:13 +0900

    feat: define data types

---
+++ src/types/data.ts
@@ -0,0 +1,60 @@
+export interface StateProps {
+  id: string;
+  name: string;
+  // component id
+  root: string;
+}
+
+export interface StateListProps {
+  total: number;
+  items: StateProps[];
+}
+
+export interface PropProps {
+  id: string;
+  name: string;
+  // component id
+  root: string;
+}
+
+export interface PropListProps {
+  total: number;
+  items: PropProps[];
+}
+
+export interface EffectProps {
+  id: string;
+  name: string;
+  // state or prop id
+  dependencyIds: string[];
+}
+
+export interface EffectListProps {
+  total: number;
+  items: EffectProps[];
+}
+
+export interface ComponentProps {
+  id: string;
+  name: string;
+  // prop id
+  props: string[];
+  // state id
+  states: string[];
+  // effect id
+  effects: string[];
+  // component id
+  children: string[];
+}
+
+export interface ComponentListProps {
+  total: number;
+  items: ComponentProps[];
+}
+
+export interface DataProps {
+  componentList: ComponentListProps;
+  states: StateListProps;
+  props: PropListProps;
+  effects: EffectListProps;
+}
