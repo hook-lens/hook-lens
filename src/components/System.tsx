bfbb8c998f09aeaedbef31386b985d04235e4c96
sgeunhi <alsrb582@snu.ac.kr>
2024-12-10 22:40:45 +0900

    feat: working customized hierarchy

--- src/components/System.tsx
+++ src/components/System.tsx
@@ -9,6 +9,11 @@ interface SystemProps {
 
 interface TreeNode {
   name: string;
+  hasStates?: boolean;
+  hasProps?: boolean;
+  states?: string[];
+  props?: string[];
+  effect?: string[];
   children?: TreeNode[];
 }
 
@@ -23,6 +28,11 @@ const System = ({ data }: SystemProps) => {
 
       const convertToTreeData = (node: HierarchyComponent): TreeNode => ({
         name: node.name,
+        hasStates: !!(node.states && node.states.length > 0),
+        hasProps: !!(node.props && node.props.length > 0),
+        states: node.states,
+        props: node.props,
+        effect: node.effects,
         children: node.children.map(convertToTreeData),
       });
 
@@ -37,16 +47,110 @@ const System = ({ data }: SystemProps) => {
     return <div>Loading tree...</div>;
   }
 
+  const renderCustomNode = ({ nodeDatum }: { nodeDatum: TreeNode }) => {
+    return (
+      <g>
+        {/* Rectangle for the Component */}
+        <rect
+          x={
+            -(nodeDatum.hasStates && nodeDatum.hasProps
+              ? 100
+              : nodeDatum.hasStates || nodeDatum.hasProps
+              ? 60
+              : 50) / 2
+          }
+          y={-25}
+          width={
+            nodeDatum.hasStates && nodeDatum.hasProps
+              ? 100
+              : nodeDatum.hasStates || nodeDatum.hasProps
+              ? 60
+              : 50
+          }
+          height={50}
+          fill="#d9d9d9"
+          stroke="none"
+          rx={10}
+          ry={10}
+        />
+        {/* Rectangle for the State */}
+        {nodeDatum.hasStates && (
+          <path
+            d={
+              nodeDatum.hasProps
+                ? // If Props also exist, State rectangle is on the right
+                  `M -0 -25
+             h 40 
+             q 10 0 10 10 
+             v 30 
+             q 0 10 -10 10 
+             h -40 
+             v -50 z`
+                : `M -0 -25
+             h 20 
+             q 10 0 10 10 
+             v 30 
+             q 0 10 -10 10 
+             h -40 
+             v -50 z`
+            }
+            fill="#34c759"
+            stroke="none"
+          />
+        )}
+        {/* Rectangle for the Props */}
+        {nodeDatum.hasProps && (
+          <path
+            d={
+              nodeDatum.hasStates
+                ? // If States also exist, Props rectangle is on the left
+                  `M -40 -25
+             q -10 0 -10 10 
+             v 30 
+             q 0 10 10 10 
+             h 40 
+             v -50 
+             h -40 z`
+                : `M -20 -25
+             q -10 0 -10 10 
+             v 30 
+             q 0 10 10 10 
+             h 40 
+             v -50 
+             h -40 z`
+            }
+            fill="#a2845e"
+            stroke="none"
+          />
+        )}
+        {/* Component name */}
+        <text
+          x={0}
+          y={-30}
+          textAnchor="middle"
+          fontSize="12px"
+          fontFamily="Arial, sans-serif"
+          fill="black"
+          stroke="none"
+          fontWeight="normal"
+        >
+          {nodeDatum.name}
+        </text>
+      </g>
+    );
+  };
+
   return (
     <div style={{ width: "100%", height: "800px" }}>
       <Tree
         data={treeData}
         orientation="horizontal"
         pathFunc="diagonal"
-        nodeSize={{ x: 200, y: 200 }}
+        nodeSize={{ x: 200, y: 100 }}
         translate={{ x: 300, y: 50 }}
         zoomable
         enableLegacyTransitions
+        renderCustomNodeElement={renderCustomNode}
       />
     </div>
   );
