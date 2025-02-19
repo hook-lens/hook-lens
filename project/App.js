01b4a86ee31b09fc92f2e6ef0a64e72b5f842233
hooklens <hooklens@hcil.snu.ac.kr>
2025-02-19 21:50:49 +0900

    Updates

---
+++ project/App.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Page/Login";
+import Home from "./Page/Home";
+import Quiz from "./Page/Quiz";
+import Register from "./Page/Register";
+import Details from "./Page/Details";
+import Layout from "./Page/Layout";
+import Liquor from "./Page/Liquor";
+import { useState } from "react";
+
+function App() {
+  const [pageCount, setPageCount] = useState(1);
+  const [currentAlcoholList, setCurrentAlcoholList] = useState([]);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [dummyAlcoholList, setDummyAlcoholList] = useState(
+    require("./Asset/dummy-alcohols.json")
+  );
+  const [currentAlcoholId, setCurrentAlcoholId] = useState("");
+  const [category, setCategory] = useState(0);
+  const [filteredAlcoholList, setFilteredAlcoholList] = useState([]);
+  const [filteredItemOffset, setFilteredItemOffset] = useState(0);
+  const [filteredItemsId, setFilteredItemsId] = useState(0);
+  const [dummyQuizList, setDummyQuizList] = useState(
+    require("./Asset/dummy-quiz.json")
+  );
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route element={<Layout />}>
+            <Route
+              path="/liquor/:id"
+              element={
+                <Liquor
+                  filteredAlcoholList={filteredAlcoholList}
+                  setFilteredAlcoholList={setFilteredAlcoholList}
+                  filteredItemOffset={filteredItemOffset}
+                  setFilteredItemOffset={setFilteredItemOffset}
+                  filteredItemsId={filteredItemsId}
+                  setFilteredItemsId={setFilteredItemsId}
+                />
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <Home
+                  pageCount={pageCount}
+                  setPageCount={setPageCount}
+                  currentAlcoholList={currentAlcoholList}
+                  setCurrentAlcoholList={setCurrentAlcoholList}
+                  itemOffset={itemOffset}
+                  setItemOffset={setItemOffset}
+                  category={category}
+                />
+              }
+            />
+            <Route
+              path="/details/:id"
+              element={
+                <Details
+                  dummyAlcoholList={dummyAlcoholList}
+                  setDummyAlcoholList={setDummyAlcoholList}
+                />
+              }
+            />
+            <Route
+              path="/quiz"
+              element={
+                <Quiz
+                  dummyQuizList={dummyQuizList}
+                  setDummyQuizList={setDummyQuizList}
+                />
+              }
+            />
+          </Route>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
